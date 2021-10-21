f83b362d0ac2201e387c663a65d544ad8c4bde5d
HuangYiHang <326623091@qq.com>
2021-10-21 13:46:11 +0800

    chore: 增加ts类型约束

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 
+interface Param {
+  name: string;
+  personId: string;
+}
 // 排除值为0，转换为boolean为false的情况
-export const isFalsey = (value) => (value === 0 ? false : !value);
+export const isFalsey = (value: any) => (value === 0 ? false : !value);
 
 // 拷贝后修改，因为在一个函数中，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
-  const result = { ...object };
+export const cleanObject = (object: Param) => {
+  const result: any = { ...object };
   Object.keys(result).forEach((key) => {
     if (isFalsey(result[key])) {
       delete result[key];
@@ -16,10 +20,10 @@ export const cleanObject = (object) => {
 };
 
 // custom hook debounce
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: Param, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
-    // 每次在value变化以后，设置一个定时器
+    // 每次在value变化以后，设置一个定时器ß
     const timer = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
